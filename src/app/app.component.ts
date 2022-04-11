d9ee0464a28df32c7adf3dfe812cb960e6e9aca8
Abishag-Maitha <abishag.maitha@student.moringaschool.com>
2022-04-11 22:43:09 +0300

    Create delete, upvote and downvote buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,4 +14,15 @@ export class AppComponent {
     new QUOTE("Money","The purpose of our lives is to be happy","Mike", new Date(),0,0),
     new QUOTE("Music","The purpose of our lives is to be happy","Pendo", new Date("2022-03-01"),0,0)
   ]
+  delete(index:number){
+    if(confirm("Do you want to delete this quote?")){
+      this.allQoutes.splice(index,1)
+    }
+    }
+  upvote(index:number){
+    this.allQoutes[index].upvote+=1 
+  }
+  downvote(index:number){
+    this.allQoutes[index].downvote+=1 
+  }
 }
