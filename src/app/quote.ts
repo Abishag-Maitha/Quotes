7467299ab4b70b6e0d5ebcfcb72d5071be650704
Abishag-Maitha <abishag.maitha@student.moringaschool.com>
2022-04-13 13:53:40 +0300

    create time pipe

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,5 +1,8 @@
 import { Title } from "@angular/platform-browser";
 export class QUOTE {
+    showdescription!:boolean
+    constructor(public theme:string, public body:string, public author:string, public date:Date, public upvote:number, public downvote:number){
+        this.showdescription=false
+    }
 
-    constructor(public theme:string, public body:string, public author:string, public date:Date, public upvote:number, public downvote:number){}
 }
